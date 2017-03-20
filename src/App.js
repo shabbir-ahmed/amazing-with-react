b71b7b4552ba908803b9e15feae3c29259b26409
Shabbir Ahmed <shabbirahmed149@gmail.com>
2017-03-20 13:54:39 +0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
-const App = () => <h1>Hello World!</h1>
+class App extends React.Component {
+    render(){
+        return React.createElement('h1', null, "Hello World!")
+    }
+}
 
 export default App
